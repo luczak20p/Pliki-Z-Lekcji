4bbcc8a1ced0aefdaa1a187a993a7ae3544bc065
luczak20p <91263539+luczak20p@users.noreply.github.com>
2022-05-09 00:29:51 +0200

    Update cztery.php

--- cztery.php
+++ cztery.php
@@ -47,9 +47,32 @@ echo "nazwisko: $this->nazwisko<br/>";
 echo "katedra: $this->katedra<br/><br/>";
 }
 }
+
+class sprzedawca extends czlowiek
+{
+private $pozycja;
+private $dzial;
+public function __construct($imie,$nazwisko,$pozycja,$dzial)
+{
+$this->imie=$imie;
+$this->nazwisko=$nazwisko;
+$this->pozycja=$pozycja;
+$this->dzial=$dzial;
+}
+public function displayData()
+{
+echo "<h3>Dane profesora:</h3>";
+echo "imie: $this->imie<br/>";
+echo "nazwisko: $this->nazwisko<br/>";
+echo "pozycja: $this->pozycja<br/><br/>";
+echo "dzial: $this->dzial<br/><br/>";
+}
+}
 // program
 $student=new student('Jan','Kowalski','350','A01');
 $student->displayData();
 $profesor=new profesor('Stefan','Nowak','Informatyka Stosowana');
 $profesor->displayData();
+$sprzedawca=new sprzedawca('Marcin','Małpa','Menedżer','Marketing');
+$sprzedawca->displayData();
 ?>
