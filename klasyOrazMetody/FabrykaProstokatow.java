058532601d5d4f2a8cec4df7b50189c2d51ac5ca
luczak20p <91263539+luczak20p@users.noreply.github.com>
2022-05-23 13:13:11 +0200

    Add files via upload

--- klasyOrazMetody/FabrykaProstokatow.java
+++ klasyOrazMetody/FabrykaProstokatow.java
@@ -5,5 +5,9 @@ public class FabrykaProstokatow {
         
         Prostokat aaa = new Prostokat(10, 5);
         Prostokat bbb = new Prostokat(4, 2);
+        System.out.println(aaa.a);
+        System.out.println(bbb.a);
+
+        System.out.println(aaa.dlugoscBokuA());
     }
 }
--- klasyOrazMetody/Prostokat.java
+++ klasyOrazMetody/Prostokat.java
@@ -1,9 +1,17 @@
 package klasyOrazMetody;
 public class Prostokat {
-    int a,b;
+    public int a,b;
     public Prostokat(int a, int b){
         //Konstruktor
         this.a=a;
         this.b=b;
+    }
+
+    public int dlugoscBokuA(){
+        return this.a;
+    }
+
+    public int dlugoscBokuB(){
+        return this.b;
     }
 }
