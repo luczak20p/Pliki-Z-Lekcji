498e9a09fbc4aad00facff68aa80e4f7b6092b76
luczak20p <91263539+luczak20p@users.noreply.github.com>
2022-05-30 12:55:15 +0200

    Add files via upload

--- TrzydziestyMaj/Main.java
+++ TrzydziestyMaj/Main.java
@@ -3,8 +3,14 @@ package TrzydziestyMaj;
 public class Main {
 
     public static void main(String[] args){
+        Person p1 = new Person("Pablo",25);
+        System.out.println(p1.name+" "+p1.age);
 
-        
+        Person p2=new Person("Michael",20);
+        System.out.println(p2.name+" "+p2.age);
+  
+        System.out.println(Person.population);
+
     }
     
 }
--- TrzydziestyMaj/Person.java
+++ TrzydziestyMaj/Person.java
@@ -3,11 +3,25 @@ package TrzydziestyMaj;
 public class Person {
     String name;
     int age;
+    //aby odwołać się do zmiennej, która nie jest statyczna wystarczy utworzenie obiektu np. p1.zmienna
+    //aby odwołać się do zmiennej statycznej piszemy nazwę klasy, a po kropce nazwe zmiennej
+    static int population=0;
     //kontruktor ma taką samą nazwęjak klasa
     Person(String name, int age){
         this.name=name;
         this.age=age;
+        population++;
+    }
+    Person(String name){
+        this.name=name;
+        age=-1;
+        population++;
+    }
 
+
+    //metody statyczne nie zmienią pól niestatycznych
+    public void inc(){
+        age++;
     }
     
 }
