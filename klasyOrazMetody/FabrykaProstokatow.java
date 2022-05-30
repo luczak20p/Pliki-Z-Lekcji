18e5c9fc7fe05027af56019e0ee3694b3c2e2080
luczak20p <91263539+luczak20p@users.noreply.github.com>
2022-05-30 11:53:13 +0200

    Add files via upload

--- klasyOrazMetody/FabrykaProstokatow.java
+++ klasyOrazMetody/FabrykaProstokatow.java
@@ -5,9 +5,25 @@ public class FabrykaProstokatow {
         
         Prostokat aaa = new Prostokat(10, 5);
         Prostokat bbb = new Prostokat(4, 2);
-        System.out.println(aaa.a);
-        System.out.println(bbb.a);
+        System.out.println(aaa.dlugoscBokuA());
+        System.out.println(bbb.dlugoscBokuB());
 
         System.out.println(aaa.dlugoscBokuA());
+
+        aaa.UstawBokA(20);
+        System.out.println(aaa.dlugoscBokuA());
+
+        Prostokat nowy = new Prostokat(5, 3, "nowy");
+
+        System.out.println(nowy.nazwaProstokata());
+        
+        nowy.test();
+        nowy.test("BBBBBBBBBBBBBBBBBBBB");
+
+        int poleProstokata = nowy.test(nowy.dlugoscBokuA(),nowy.dlugoscBokuB());
+
+        System.out.println(poleProstokata);
+
+        
     }
 }
--- klasyOrazMetody/Prostokat.java
+++ klasyOrazMetody/Prostokat.java
@@ -1,12 +1,23 @@
 package klasyOrazMetody;
+// package - po polsku pakiet
 public class Prostokat {
-    public int a,b;
+    private int a,b;
+    private String NazwaProstokata;
     public Prostokat(int a, int b){
         //Konstruktor
         this.a=a;
         this.b=b;
     }
 
+
+    //stworzymy kolejny konstruktor rozszerzający nazwe starego konstruktora(przeciążenia konstruktora)
+    public Prostokat(int a, int b, String NazwaProstokata){
+        //Konstruktor
+        this.a=a;
+        this.b=b;
+        this.NazwaProstokata=NazwaProstokata;
+    }
+    
     public int dlugoscBokuA(){
         return this.a;
     }
@@ -14,4 +25,36 @@ public class Prostokat {
     public int dlugoscBokuB(){
         return this.b;
     }
+
+    public void UstawBokA(int a){
+        this.a=a;
+    }
+
+    public void UstawBokB(int b){
+        this.b=b;
+    }
+
+    public String nazwaProstokata(){
+        return this.NazwaProstokata;
+    }
+
+    public void UstawNazwa(String nazwa){
+        this.NazwaProstokata=nazwa;
+    }
+
+    public void test(){
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
+    }
+
+    public void test(String tekst){
+        System.out.println(tekst);
+    }
+
+    public int test(int a, int b){
+        return a*b;
+    }
+
+
+
 }
+
